d3e69371f74e0fb273e299547aac19bb8a47655f
vaishuss <sdk9750@gmail.com>
2024-07-21 16:30:33 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,12 @@
+function calculateAge() {
+    var birthdate = document.getElementById("birthdate").value;
+    var today = new Date();
+    var birthDate = new Date(birthdate);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    document.getElementById("result").innerHTML = "Your age is: " + age;
+}
